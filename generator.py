81abddb4ab8f1b5f5458432023c792a422a9ee25
AsterisCh <asteriosc@csd.auth.gr>
2016-06-09 07:23:31 +0300

    Fix output & input format

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 import random
 
 # open file with users
-input="users"
+input="users.txt"
 spaced_users = open(input,"r")
 # init userlist taken from file and the generated dictionary [user:{ list of user's friends }]
 userlist = []
@@ -13,8 +13,9 @@ for user in users:
     friends_map[user]=[]
 spaced_users.close()  # close input file
 # Generate a random graph of users and their friends
+print str(len(userlist))+"\n"
 for user in userlist:
-    numberoffriends=random.randint(1,len(userlist)/2-1)     # Pick a random number of friends for each user
+    numberoffriends=random.randint(1,len(userlist)-1)     # Pick a random number of friends for each user
     rsample=random.sample(userlist,numberoffriends)         # Get a random sample from userlist with size realted to the picked random number
     for friend in rsample:      # Add this sample from userlist to user's friends
         if friend!=user:        # Do not add user to user's friend!
@@ -33,12 +34,14 @@ for user in userlist:
             if not flag:
                 friends_map[friend].append(user)    # Add user to friend's friends!
 # open output file for writing
-output="friends_graph"
+output="friends_graph.txt"
 friends=open(output,"w")
-#write output file
+#write output file in a format of [ user friend_A, friends_B, ..., friend_N ]
 for user in friends_map.keys():
     friends.write(user+" ")
     for users_friend in friends_map[user]:
-        friends.write(users_friend+" ")
+        friends.write(users_friend+", ")
+    friends.seek(friends.tell()-2)
+    friends.truncate()
     friends.write("\n")
 friends.close()
